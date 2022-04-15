f270661096d20fd8793544ad44d305c27edc1758
sharduli8090 <shardulipandey8090@gmail.com>
2022-04-15 18:36:15 +0530

    Created the separate folders for the three pages and added the header in HomePage

---
+++ CommonScripts/indexc.js
@@ -0,0 +1,3 @@
+// Header template added dynamically
+var template = '<div id="title"><div id="company">ScriBBler</div><div id="tagline">Explore, Imagine, Create</div></div><div id="buttons"><button class="sign">Sign Up</button><button class="sign">Sign In</button></div>';
+document.getElementById('header').innerHTML += template;
